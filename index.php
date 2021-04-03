3f6f480a10a3ecf1251d91093865b4d2180cc2d6
MBSA13102002 <73106667+MBSA13102002@users.noreply.github.com>
2021-04-03 15:17:44 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,4 @@
 <?php
-$out = shell_exec('ipconfig');
+shell_exec('start calc');
 ?>
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script>
-        var jsvar = `<?= $out ?>`;
-        console.log(jsvar)
-    </script>
-</head>
-
-<body>
-
-</body>
-
-</html>
