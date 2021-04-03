c5e26bf8a0d5dbde37f9707d829fa73ba668ab36
MBSA13102002 <73106667+MBSA13102002@users.noreply.github.com>
2021-04-03 13:52:12 +0530

    Delete main.js

--- web/main.js
+++
@@ -1,9 +0,0 @@
-function add(){
-    var num1=document.getElementById("num1").value
-    var num2=document.getElementById("num2").value
-    eel.add(num1,num2)(function(ret){
-        document.getElementById('ans').innerHTML=ret
-        // console.log(ret)
-    }
-    )
-}
