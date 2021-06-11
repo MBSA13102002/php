c0c4d9fd2314d3d495e2d31dcca16241dc00e3ea
MBSA13102002 <MBSA13102002@GMAIL.COM>
2021-06-11 19:55:59 +0530

    commit

--- NumberAuthentication.js
+++ NumberAuthentication.js
@@ -8,6 +8,7 @@ function render() {
 function phoneAuth() {
     //get the number
     var number=document.getElementById('number').value;
+    localStorage.setItem("mob_no", number);
     //phone number authentication function of firebase
     //it takes two parameter first one is number,,,second one is recaptcha
     firebase.auth().signInWithPhoneNumber(number,window.recaptchaVerifier).then(function (confirmationResult) {
@@ -15,18 +16,35 @@ function phoneAuth() {
         window.confirmationResult=confirmationResult;
         coderesult=confirmationResult;
         console.log(coderesult);
-        alert("Message sent");
+        document.getElementById(message).innerHTML="OTP SENT"
+        document.getElementById('send_otp').style.display="none";
+        document.getElementById('confirm_otp').style.display="block";
+        // alert("Message sent");
     }).catch(function (error) {
-        alert(error.message);
+        document.getElementById("message").innerHTML=error.message;
+        // alert(error.message);
     });
 }
 function codeverify() {
     var code=document.getElementById('verificationCode').value;
     coderesult.confirm(code).then(function (result) {
-        alert("Successfully registered");
+        // alert("Successfully registered");
+        document.getElementById("message").innerHTML="OTP VERIFIED!!!!";
         var user=result.user;
         console.log(user);
     }).catch(function (error) {
-        alert(error.message);
+        document.getElementById("message").innerHTML=error.message;
+        // alert(error.message);
     });
 }
+function send_data(){
+    
+    var name=document.getElementById("name").value;
+    var email=document.getElementById("email").value;
+    var number=localStorage.getItem('mob_no')
+    formMessage.push({
+        name:name,
+        email:email,
+        number:number});
+
+};
