711ab324582ab25b92aa55a14f1ce1533b9f881d
MBSA-INFINITY <mbsaiaditya@gmail.com>
2021-06-18 17:41:44 +0530

    commit

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ function Send_OTP() {
     var Email = document.getElementById('email').value
     var Number = document.getElementById('phone_number').value
     var Choice = document.getElementById('type_flats').value
-    if (Name != "" && Email != "" && Number != "" && Choice!="CHOOSE YOUR REQUIREMENT") {
+    if (Name != "" && Email != "" && Number != "" && Choice != "CHOOSE YOUR REQUIREMENT") {
 
         var xmlHttp = new XMLHttpRequest();
         theUrl = "https://2factor.in/API/V1/" + "16f346f4-cad2-11eb-8089-0200cd936042" + "/SMS/" + Number + "/" + Math.floor(1000 + Math.random() * 9000).toString()
@@ -45,7 +45,7 @@ function Popup_Send_OTP() {
     var Email = document.getElementById('popup_email').value
     var Number = document.getElementById('popup_phone_number').value
     var Choice = document.getElementById('popup_type_flats').value
-    if (Name != "" && Email != "" && Number != "" && Choice!="CHOOSE YOUR REQUIREMENT") {
+    if (Name != "" && Email != "" && Number != "" && Choice != "CHOOSE YOUR REQUIREMENT") {
 
         var xmlHttp = new XMLHttpRequest();
         theUrl = "https://2factor.in/API/V1/" + "16f346f4-cad2-11eb-8089-0200cd936042" + "/SMS/" + Number + "/" + Math.floor(1000 + Math.random() * 9000).toString()
@@ -97,7 +97,7 @@ function Confirm_OTP() {
     today = dd + '/' + mm + '/' + yyyy;
     time = hour + ":" + min + ":" + sec
 
-    if (Name != '' && Email != '' && Number != '' && Verification_Code != '' && Choice!="CHOOSE YOUR REQUIREMENT") {
+    if (Name != '' && Email != '' && Number != '' && Verification_Code != '' && Choice != "CHOOSE YOUR REQUIREMENT") {
         var xmlHttp = new XMLHttpRequest();
         theUrl = "https://2factor.in/API/V1/" + "16f346f4-cad2-11eb-8089-0200cd936042" + "/SMS/VERIFY/" + session_id + "/" + Verification_Code
         // theUrl="http://2factor.in/API/V1/293832-67745-11e5-88de-5600000c6b13/SMS/9911991199/4499"
@@ -115,7 +115,7 @@ function Confirm_OTP() {
                 Number: Number,
                 OTP: Verification_Code,
                 date: today,
-                time:time,
+                time: time,
                 Choice: Choice
             });
             alert("Our expert will get in touch with you shortly!!")
@@ -124,9 +124,9 @@ function Confirm_OTP() {
             document.getElementById("phone_number").value = ''
             document.getElementById("type_flats").value = "CHOOSE YOUR REQUIREMENT"
             document.getElementById("confirm_otp").style.display = "none";
-            
-            
-          
+
+
+
         }
         else {
             alert("ERROR OCCURED")
@@ -158,7 +158,7 @@ function Popup_Confirm_OTP() {
     today = dd + '/' + mm + '/' + yyyy;
     time = hour + ":" + min + ":" + sec
 
-    if (Name != '' && Email != '' && Number != '' && Verification_Code != '' && Choice!="CHOOSE YOUR REQUIREMENT") {
+    if (Name != '' && Email != '' && Number != '' && Verification_Code != '' && Choice != "CHOOSE YOUR REQUIREMENT") {
         var xmlHttp = new XMLHttpRequest();
         theUrl = "https://2factor.in/API/V1/" + "16f346f4-cad2-11eb-8089-0200cd936042" + "/SMS/VERIFY/" + session_id + "/" + Verification_Code
         // theUrl="http://2factor.in/API/V1/293832-67745-11e5-88de-5600000c6b13/SMS/9911991199/4499"
@@ -176,7 +176,7 @@ function Popup_Confirm_OTP() {
                 Number: Number,
                 OTP: Verification_Code,
                 date: today,
-                time:time,
+                time: time,
                 Choice: Choice
             });
             alert("Our expert will get in touch with you shortly!!")
@@ -185,9 +185,9 @@ function Popup_Confirm_OTP() {
             document.getElementById("popup_phone_number").value = ''
             document.getElementById("popup_type_flats").value = "CHOOSE YOUR REQUIREMENT"
             document.getElementById("popup_confirm_otp").style.display = "none";
-            
-            
-          
+
+
+
         }
         else {
             alert("ERROR OCCURED")
@@ -247,35 +247,63 @@ function Popup_Confirm_OTP() {
 // }
 
 
-function Splash(){
-    setTimeout(function(){  
-    document.getElementById("main_content").classList.add("main_imp");
-    document.getElementById("splash").style.display="block"; 
-    document.getElementById("splash").style.top=window.pageYOffset.toString()+'px'; 
-    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-  
-        // if any scroll is attempted, set this to the previous value
-        window.onscroll = function() {
-            window.scrollTo(scrollLeft, scrollTop);
-        };
-}, 3000);
-   
+function Splash() {
+    setTimeout(function () {
+        document.getElementById("main_content").classList.add("main_imp");
+        document.getElementById("splash").style.display = "block";
+        document.getElementById("splash").style.top = window.pageYOffset.toString() + 'px';
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+
+            // if any scroll is attempted, set this to the previous value
+            window.onscroll = function () {
+                window.scrollTo(scrollLeft, scrollTop);
+            };
+    }, 3000);
+
 }
 function removed_done() {
-   document.getElementById("splash").classList.add("animate__zoomOutDown");
-   setTimeout(function(){  
-    document.getElementById("main_content").classList.remove("main_imp");
-    document.getElementById("splash").style.display="none";
-}, 1000);
+    var Name = document.getElementById("popup_name").value
+    var Email = document.getElementById('popup_email').value
+    var Number = document.getElementById('popup_phone_number').value
+    var Choice = document.getElementById('popup_type_flats').value
+    var code_display = document.getElementById("popup_confirm_otp").style.display
+    if (code_display == 'none') {
+        if (Name != '' || Email != '' || Number != '' || Choice != "CHOOSE YOUR REQUIREMENT") {
+            var ref = confirm("Do you really want to exit the enquiry?")
+            if (ref == true) {
+                document.getElementById("splash").classList.add("animate__zoomOutDown");
+                setTimeout(function () {
+                    document.getElementById("main_content").classList.remove("main_imp");
+                    document.getElementById("splash").style.display = "none";
+                }, 1000);
+                window.onscroll = function () { };
+
+            } else {
+
+            }
+         
+        }
+        else {
+            document.getElementById("splash").classList.add("animate__zoomOutDown");
+            setTimeout(function () {
+                document.getElementById("main_content").classList.remove("main_imp");
+                document.getElementById("splash").style.display = "none";
+            }, 1000);
+            window.onscroll = function () { };
+        }
+    }
+    else{
+        alert("Verfication under process..Enter the OTP & Submit the form!!!")
+    }
+
    
-   window.onscroll = function() {};
 }
-function sai(){
-    // var scrollPos = window.scrollY || window.scrollTop || document.getElementsByTagName("html")[0].scrollTop;
-    var sai = window.pageYOffset
-    alert(sai)
-}
+// function sai() {
+//     // var scrollPos = window.scrollY || window.scrollTop || document.getElementsByTagName("html")[0].scrollTop;
+//     var sai = window.pageYOffset
+//     alert(sai)
+// }
 // function submit() {
 //     var Name = document.getElementById("name").value
 //     var Email = document.getElementById('email').value
