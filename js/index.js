85520e1d0801664ea31fb20f67f98d83225f6c8b
MBSA-INFINITY <mbsaiaditya@gmail.com>
2021-06-19 13:11:47 +0530

    commit

--- js/index.js
+++ js/index.js
@@ -21,9 +21,50 @@ function Send_OTP() {
             // localStorage.setItem("phone_num", Number)
             // localStorage.setItem("Name", Name)
             // localStorage.setItem("Email", Email)
-            alert("OTP HAS BEEN SENT TO YOUR MOBILE NUMBER")
+            // alert("OTP HAS BEEN SENT TO YOUR MOBILE NUMBER")
             document.getElementById('confirm_otp').style.display = "block";
             document.getElementById('block_chain').style.display = "none";
+            // document.getElementById('send_otp').style.display="none"; 
+        }
+        else {
+            alert("ERROR OCCURED")
+        }
+
+    }
+    else {
+        alert("FIRST FILL ALL THE DETAILS")
+    }
+
+    // console.log(stat,session_id)
+    // console.log(xmlHttp.responseText);
+
+}
+
+function New_Send_OTP() {
+    var Name = document.getElementById('new_name').value
+    var Email = document.getElementById('new_email').value
+    var Number = document.getElementById('new_phone_number').value
+    var Choice = document.getElementById('new_type_flats').value
+    if (Name != "" && Email != "" && Number != "" && Choice != "CHOOSE YOUR REQUIREMENT") {
+
+        var xmlHttp = new XMLHttpRequest();
+        theUrl = "https://2factor.in/API/V1/" + "16f346f4-cad2-11eb-8089-0200cd936042" + "/SMS/" + Number + "/" + Math.floor(1000 + Math.random() * 9000).toString()
+        // theUrl="http://2factor.in/API/V1/293832-67745-11e5-88de-5600000c6b13/SMS/9911991199/4499"
+        xmlHttp.open("GET", theUrl, false); // false for synchronous request
+        xmlHttp.send(null);
+        result = xmlHttp.responseText;
+        var res = result.split(",");
+        stat = res[0].slice(11, res[0].length - 1);
+        detail = res[1].slice(11, res[1].length - 2);
+        window.session_id = detail;
+
+        if (stat == "Success") {
+            // localStorage.setItem("phone_num", Number)
+            // localStorage.setItem("Name", Name)
+            // localStorage.setItem("Email", Email)
+            // alert("OTP HAS BEEN SENT TO YOUR MOBILE NUMBER")
+            document.getElementById('new_confirm_otp').style.display = "block";
+            document.getElementById('new_block_chain').style.display = "none";
             // document.getElementById('send_otp').style.display="none"; 
         }
         else {
@@ -62,7 +103,7 @@ function Popup_Send_OTP() {
             // localStorage.setItem("phone_num", Number)
             // localStorage.setItem("Name", Name)
             // localStorage.setItem("Email", Email)
-            alert("OTP HAS BEEN SENT TO YOUR MOBILE NUMBER")
+            // alert("OTP HAS BEEN SENT TO YOUR MOBILE NUMBER")
             document.getElementById('popup_confirm_otp').style.display = "block";
             document.getElementById('popup_block_chain').style.display = "none";
             // document.getElementById('send_otp').style.display="none"; 
@@ -124,6 +165,67 @@ function Confirm_OTP() {
             document.getElementById("phone_number").value = ''
             document.getElementById("type_flats").value = "CHOOSE YOUR REQUIREMENT"
             document.getElementById("confirm_otp").style.display = "none";
+
+
+
+        }
+        else {
+            alert("ERROR OCCURED")
+
+        }
+    }
+    else {
+        alert("FILL OUT ALL THE DETAILS OF THE FORM AND VERIFY YOUR PHONE NUMBER AS WELL")
+    }
+
+
+
+
+}
+function New_Confirm_OTP() {
+
+    var Name = document.getElementById("new_name").value
+    var Email = document.getElementById('new_email').value
+    var Number = document.getElementById('new_phone_number').value
+    var Verification_Code = document.getElementById('new_verificationCode').value
+    var Choice = document.getElementById('new_type_flats').value
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+    var hour = today.getHours()
+    var min = today.getMinutes()
+    var sec = today.getSeconds()
+    today = dd + '/' + mm + '/' + yyyy;
+    time = hour + ":" + min + ":" + sec
+
+    if (Name != '' && Email != '' && Number != '' && Verification_Code != '' && Choice != "CHOOSE YOUR REQUIREMENT") {
+        var xmlHttp = new XMLHttpRequest();
+        theUrl = "https://2factor.in/API/V1/" + "16f346f4-cad2-11eb-8089-0200cd936042" + "/SMS/VERIFY/" + session_id + "/" + Verification_Code
+        // theUrl="http://2factor.in/API/V1/293832-67745-11e5-88de-5600000c6b13/SMS/9911991199/4499"
+        xmlHttp.open("GET", theUrl, false); // false for synchronous request
+        xmlHttp.send(null);
+        result = xmlHttp.responseText
+        var res = result.split(",");
+        stat = res[0].slice(11, res[0].length - 1)
+        // detail = res[1].slice(10,res[1].length-1)
+        if (stat == "Success") {
+            let formMessage = firebase.database().ref("FORM DATA");
+            formMessage.push({
+                Name: Name,
+                Email: Email,
+                Number: Number,
+                OTP: Verification_Code,
+                date: today,
+                time: time,
+                Choice: Choice
+            });
+            alert("Our expert will get in touch with you shortly!!")
+            document.getElementById("new_name").value = ''
+            document.getElementById("new_email").value = ''
+            document.getElementById("new_phone_number").value = ''
+            document.getElementById("new_type_flats").value = "CHOOSE YOUR REQUIREMENT"
+            document.getElementById("new_confirm_otp").style.display = "none";
 
 
 
@@ -203,50 +305,6 @@ function Popup_Confirm_OTP() {
 
 }
 
-// function submit() {
-//     var Name = document.getElementById("name").value
-//     var Email = document.getElementById('email').value
-//     var Number = document.getElementById('phone_number').value
-//     var Verification_Code = document.getElementById('verificationCode').value
-//     var today = new Date();
-//     var dd = String(today.getDate()).padStart(2, '0');
-//     var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-//     var yyyy = today.getFullYear();
-//     today = dd + '/' + mm + '/' + yyyy;
-//     if (confirmation != null) {
-//         if (Name != '' && Email != '' && Number != '' && Verification_Code != '') {
-//             let formMessage = firebase.database().ref("FORM DATA");
-//             formMessage.push({
-//                 Name: Name,
-//                 Email: Email,
-//                 Number: Number,
-//                 OTP: Verification_Code,
-//                 date: today
-//             });
-
-//             alert("Form Submitted Successfully")
-//             document.getElementById("name").value = ''
-//             document.getElementById("email").value = ''
-//             document.getElementById("phone_number").value = ''
-//             document.getElementById("confirm_otp").style.display = "none";
-
-
-//         }
-//         else {
-//             alert("FIIL OUT ALL THE DETAILS OF THE FORM AND VERIFY YOUR PHONE NUMBER AS WELL")
-//             //    location.reload()
-//         }
-//     }
-//     else {
-//         alert("Please confirm the otp and check whether all details are filled or not")
-//     }
-// }
-// function scrollwin() {
-//     window.scrollTo(0, 750);
-//     alert("Fill the Form to Contact our Experts")
-// }
-
-
 function Splash() {
     setTimeout(function () {
         document.getElementById("main_content").classList.add("main_imp");
@@ -299,47 +357,3 @@ function removed_done() {
 
    
 }
-// function sai() {
-//     // var scrollPos = window.scrollY || window.scrollTop || document.getElementsByTagName("html")[0].scrollTop;
-//     var sai = window.pageYOffset
-//     alert(sai)
-// }
-// function submit() {
-//     var Name = document.getElementById("name").value
-//     var Email = document.getElementById('email').value
-//     var Number = document.getElementById('phone_number').value
-//     var Verification_Code = document.getElementById('verificationCode').value
-//     var today = new Date();
-//     var dd = String(today.getDate()).padStart(2, '0');
-//     var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-//     var yyyy = today.getFullYear();
-//     today = dd + '/' + mm + '/' + yyyy;
-//     if (confirmation != null) {
-//         if (Name != '' && Email != '' && Number != '' && Verification_Code != '') {
-//             let formMessage = firebase.database().ref("FORM DATA");
-//             formMessage.push({
-//                 Name: Name,
-//                 Email: Email,
-//                 Number: Number,
-//                 OTP: Verification_Code,
-//                 date: today
-//             });
-
-//             alert("Form Submitted Successfully")
-//             document.getElementById("name").value = ''
-//             document.getElementById("email").value = ''
-//             document.getElementById("phone_number").value = ''
-//             document.getElementById("confirm_otp").style.display = "none";
-
-
-//         }
-//         else {
-//             alert("FIIL OUT ALL THE DETAILS OF THE FORM AND VERIFY YOUR PHONE NUMBER AS WELL")
-//             //    location.reload()
-//         }
-//     }
-//     else {
-//         alert("Please confirm the otp and check whether all details are filled or not")
-//     }
-// }
-
